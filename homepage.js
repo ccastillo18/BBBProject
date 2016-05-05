097fd6a0a6de5eba1ba835bf5f5deb093f670cd5
ccastillo18 <ccastillo@cristoreyny.org>
2016-05-05 12:57:01 +0000

    We finshed the API

---
+++ homepage.js
@@ -0,0 +1,33 @@
+
+$(document).ready(function() {
+    SC.initialize({
+    client_id: '2c79ca06bff1ac6c428f19600e3ccdd4'
+    });
+    playTrack("261372196");
+    
+});
+
+
+function searchTracks(mood) {
+  SC.get('/tracks', {
+    tags: mood,
+  }).then(function(tracks) {
+    console.log(tracks);
+    playTrack(tracks[0].id);
+  });
+
+}
+
+var currentSong= null;
+
+
+function playTrack(trackid) {
+  SC.stream('/tracks/' + trackid).then(function(player) {
+    if (currentSong) {
+      currentSong.pause();
+    }
+    currentSong= player;
+    currentSong.play();
+  });
+  
+}
